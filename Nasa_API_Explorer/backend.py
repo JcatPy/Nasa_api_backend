3bd9b213dc06c51eeb6f16f655d236857bdcbd09
Jolin Lodhiya <lodhiya@uwindsor.ca>
2024-12-31 16:03:06 -0500

    First commit on this project

---
+++ Nasa_API_Explorer/backend.py
@@ -0,0 +1,29 @@
+from flask import request, jsonify
+from . import app
+from .models import User
+from . import db
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        try:
+            data = request.json
+            if not data or 'username' not in data:
+                return jsonify({'error': 'Username is required'}), 400 # Bad request
+
+            username = data['username']
+            password = data['password']
+            user = User(username =  username, password = password)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({'message': f"Successfully signed up {username}"}), 200 # OK
+
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500 # Internal server error
+
+    # Handle GET request
+    elif request.method == 'GET':
+        # You can return a simple message or some default data for the GET request
+        return jsonify({'message': 'Welcome to the sign-up page. Please send a POST request to sign up.'})
+
