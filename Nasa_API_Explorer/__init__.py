53ca6488abbac1ab137bfbed14a9c55346378b21
Jolin Lodhiya <lodhiya@uwindsor.ca>
2025-01-04 02:43:44 -0500

    added login logic and also updated sign_up

--- Nasa_API_Explorer/__init__.py
+++ Nasa_API_Explorer/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 CORS(app, resources={r"/sign_up": {"origins": '*'}}, supports_credentials=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # SQLite database URI
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app) # login manager which handles all the sessions and cookies
 
 from . import backend
--- Nasa_API_Explorer/backend.py
+++ Nasa_API_Explorer/backend.py
@@ -2,7 +2,7 @@ from flask import request, jsonify
 from . import app
 from .models import User
 from . import db
-
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
@@ -11,6 +11,9 @@ def sign_up():
             data = request.json
             if not data or 'username' not in data:
                 return jsonify({'error': 'Username is required'}), 400 # Bad request
+
+            elif data['username'] == User.query.filter_by(email=data['username']).first():
+                return jsonify({'error': 'Username already exists'}), 400 # Bad request
 
             username = data['username']
             password = data['password']
@@ -32,12 +35,27 @@ def sign_up():
 def login():
     if request.method == 'POST':
         try:
-            data = request.json
+            data = request.json # Get the JSON data from the request
             if not data or 'username' not in data:
                 return jsonify({'error': 'Username is required'}), 400  # Bad request
 
+            username = data['username']
+            password = data['password']
+            user = User.query.filter_by(email=username).first() # Query the database for the user
+            if user and user.password == password:  # Check if the user exists and the password is correct
+                login_user(user)
+                return jsonify({'message': f"{username} successfully logged in"}), 200 # OK
+            else:
+                return jsonify({'error': 'Invalid username or password'}), 401
         except Exception as e:
             return jsonify({'error': str(e)}), 500 # Internal server error
 
     elif request.method == 'GET':
         return jsonify({'message': 'Welcome to the login page. Please send a POST request to log in.'})
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Successfully logged out'}), 200
+
