c861ea43b5166f5933b92dd4d17d55b04f8a29ea
Jolin Lodhiya <lodhiya@uwindsor.ca>
2025-01-06 23:55:17 -0500

    added NASA EONET API

---
+++ Nasa_API_Explorer/Nasa_EONET.py
@@ -0,0 +1,28 @@
+import requests
+import os
+
+api_key = 'b5Dn6cS94857ydQGnVWTTNT8fCIAmLtNAsyalhlo'
+
+def get_natural_disasters_by_events(limit=10, days=6 ):
+    list_of_natural_disasters = []
+    event_description = {}
+
+    # get the url for the eonet api to get the natural disasters according to the givine limit and days
+    url = (f'https://eonet.gsfc.nasa.gov/api/v2.1/events?api_key={api_key}&source=InciWeb,Eo&status=closed'
+           f'&limit={limit}&days{days}'
+           )
+
+    response = requests.get(url)  # Send a GET request to the URL
+    natural_disaster_by_events = response.json()  # Parse the JSON response into a Python dictionary
+
+    for event_data in natural_disaster_by_events['events']:
+        event_description = {
+            'title': event_data['title'],
+            'description': event_data['description'],
+        }
+        list_of_natural_disasters.append(event_description)
+
+    return list_of_natural_disasters
+
+l1 = get_natural_disasters_by_events()
+print(l1)
